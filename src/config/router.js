9717d12fa66fb9eb07d4d58b69ab2d770e614bf0
takuntohmartin <takuntohmartin@gmail.com>
2020-05-13 19:51:58 +0100

    adding routes to app

--- src/config/router.js
+++ src/config/router.js
@@ -1,6 +1,29 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import { HomePage,SignupPage,ReportPage,PageNotFound } from '../components/'
 
 Vue.use(Router);
 export default new Router({
+    routes:[
+        {
+            name:'HomePage',
+            path:'/',
+            component: HomePage
+        },
+        {
+            name:'ReportPage',
+            path:'/report',
+            component:ReportPage
+        },
+        {
+            name:'SignupPage',
+            path:'/login',
+            component:SignupPage
+        },
+        {
+            path: '**',
+            name: '404',
+            component: PageNotFound
+      }
+    ]
 })
